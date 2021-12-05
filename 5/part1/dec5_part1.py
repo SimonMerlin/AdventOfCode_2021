9dcdf4e09943ae0e398d254e59bbc0cd250b5109
merlin <merlinsimon98@gmail.com>
2021-12-05 12:37:48 +0100

    add answers to day 5

---
+++ 5/part1/dec5_part1.py
@@ -0,0 +1,32 @@
+f = open('data.txt', 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+data = dict()
+
+for line in lines:
+    start, end = line.split('->')
+    startx, starty = start.split(',')
+    startx, starty = int(startx.strip()), int(starty.strip())
+
+    endx, endy = end.split(',')
+    endx, endy = int(endx.strip()), int(endy.strip())
+
+    if startx == endx: # vertical line
+        if starty > endy:
+            starty, endy = endy, starty
+        for i in range(starty, endy+1, 1):
+            data[startx] = data.get(startx, dict())
+            data[startx][i] = data.get(startx).get(i, 0) +1
+    elif starty == endy: # horizontal line
+        if startx > endx:
+            startx, endx = endx, startx
+        for i in range(startx, endx+1, 1):
+            data[i] = data.get(i, dict())
+            data[i][starty] = data.get(i).get(starty, 0) +1
+
+cpt = 0
+for xkey in data.keys():
+    for ykey in  data.get(xkey).keys():
+        if data[xkey][ykey] >=2:
+            cpt+=1
+print(cpt)
